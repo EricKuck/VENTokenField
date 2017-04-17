f194b41d5f22f850bec865b63857b0a767017772
Eric Kuck <eric@bluelinelabs.com>
2017-04-17 15:51:23 -0500

    Fixed token padding

--- VENTokenField/VENTokenField.m
+++ VENTokenField/VENTokenField.m
@@ -241,6 +241,7 @@ static const CGFloat VENTokenFieldDefaultMaxHeight          = 150.0;
     CGFloat currentY = 0;
 
     [self layoutToLabelInView:self.scrollView origin:CGPointZero currentX:&currentX];
+    currentX += self.tokenPadding;
     [self layoutTokensWithCurrentX:&currentX currentY:&currentY];
     [self layoutInputTextFieldWithCurrentX:&currentX currentY:&currentY clearInput:shouldAdjustFrame];
 
